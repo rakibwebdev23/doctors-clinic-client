52b0682fa4c612a2d73cc915013a3c3f39c93f52
Rakib <rh.rakibhasan365@gmail.com>
2024-12-15 19:44:49 +0600

    doctors details set responsive

--- src/component/DoctorDetails/DoctorDetails.jsx
+++ src/component/DoctorDetails/DoctorDetails.jsx
@@ -9,7 +9,7 @@ const DoctorDetails = ({ doctor }) => {
     const { _id, name, image, specialist, email, visitFee, rating, chamberLocation } = doctor;
 
     return (
-        <div className="bg-white shadow-lg rounded-lg hover:shadow-xl transition-all duration-300 ease-in-out transform hover:scale-105">
+        <div className="bg-white shadow-lg rounded-lg transition-all duration-300 ease-in-out hover:bg-blue-100 hover:shadow-xl">
             <figure className="w-full h-56 overflow-hidden rounded-t-lg">
                 <img
                     src={image}
@@ -17,7 +17,7 @@ const DoctorDetails = ({ doctor }) => {
                     className="w-full h-full object-cover transition-transform duration-500 ease-in-out hover:scale-110"
                 />
             </figure>
-            <div className="p-6">
+            <div className="p-6 text-left"> {/* Added text-left to align text to the left */}
                 <h3 className="text-2xl font-semibold text-gray-800">{name}</h3>
                 <p className="text-lg text-gray-500 mb-3">{specialist}</p>
 
@@ -43,7 +43,6 @@ const DoctorDetails = ({ doctor }) => {
                         <span>${visitFee}</span>
                     </div>
                 </div>
-
                 <div className="mt-6">
                     <Link to={`/doctors/${_id}`}>
                         <button className="btn bg-blue-500 hover:bg-blue-600 text-white w-full py-3 rounded-md transition-all duration-300">
