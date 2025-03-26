58e3165eb5a0cd724cdcb6ee003208a1b1fbb73c
Rakib <rh.rakibhasan365@gmail.com>
2025-03-26 19:20:28 +0600

    service component add container

--- src/pages/DoctorsList/Doctors/Doctors.jsx
+++ src/pages/DoctorsList/Doctors/Doctors.jsx
@@ -6,13 +6,14 @@ import BannerShared from '../../Shared/BannerShared/BannerShared';
 import img from "../../../assets/images/doctor4 (2).jpg"
 import { useState } from 'react';
 import HelmetProvide from '../../../component/HelmetProvide';
+import Container from '../../../component/Container/Container';
 
 const Doctors = () => {
 
     const [tabIndex, setTabIndex] = useState(0);
 
     const [doctorsSpecial, doctorsCardiologist, doctorsDermatologist, doctorsOrthopedic, doctorsNeurologist, doctorsEndocrinologist, doctorsGynecologist] = useDoctorsList();
-    
+
 
     return (
         <div>
@@ -22,55 +23,57 @@ const Doctors = () => {
                 subTitle={"Our Doctors"}
                 description={"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi asperiores quaerat, labore esse officia, exercitationem corporis repellat nostrum repudiandae architecto cum amet! Excepturi asperiores quaerat, labore esse officia, exercitationem corporis repellat nostrum repudiandae architecto cum amet! Quam accusamus, totam fugiat voluptas."}
             ></BannerShared>
-            <div>
-                <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)
-                } className="text-center">
-                    <TabList className="font-bold uppercase border-black p-4 text-white flex lg:flex-row flex-col lg:justify-center text-left gap-2 py-6 mb-4">
-                        <Tab className="p-4 bg-blue-700 hover:bg-gray-300 cursor-pointer rounded hover:text-black hover:delay-100 duration-300">expert</Tab>
-                        <Tab className="p-4 bg-blue-700 hover:bg-gray-300 cursor-pointer rounded hover:text-black hover:delay-100 duration-300">cardiologist</Tab>
-                        <Tab className="p-4 bg-blue-700 hover:bg-gray-300 cursor-pointer rounded hover:text-black hover:delay-100 duration-300">dermatologist</Tab>
-                        <Tab className="p-4 bg-blue-700 hover:bg-gray-300 cursor-pointer rounded hover:text-black hover:delay-100 duration-300">orthopedic Surgeon</Tab>
-                        <Tab className="p-4 bg-blue-700 hover:bg-gray-300 cursor-pointer rounded hover:text-black hover:delay-100 duration-300">neurologist</Tab>
-                        <Tab className="p-4 bg-blue-700 hover:bg-gray-300 cursor-pointer rounded hover:text-black hover:delay-100 duration-300">endocrinologist</Tab>
-                        <Tab className="p-4 bg-blue-700 hover:bg-gray-300 cursor-pointer rounded hover:text-black hover:delay-100 duration-300">gynecologist</Tab>
-                    </TabList>
-                    <TabPanel>
-                        <div>
-                            <DoctorsList doctors={doctorsSpecial}></DoctorsList>
-                        </div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div>
-                            <DoctorsList doctors={doctorsCardiologist}></DoctorsList>
-                        </div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div>
-                            <DoctorsList doctors={doctorsDermatologist}></DoctorsList>
-                        </div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div>
-                            <DoctorsList doctors={doctorsOrthopedic}></DoctorsList>
-                        </div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div>
-                            <DoctorsList doctors={doctorsNeurologist}></DoctorsList>
-                        </div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div>
-                            <DoctorsList doctors={doctorsEndocrinologist}></DoctorsList>
-                        </div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div>
-                            <DoctorsList doctors={doctorsGynecologist}></DoctorsList>
-                        </div>
-                    </TabPanel>
-                </Tabs>
-            </div>
+            <Container>
+                <div>
+                    <Tabs defaultIndex={tabIndex} onSelect={(index) => setTabIndex(index)
+                    } className="text-center">
+                        <TabList className="font-bold uppercase border-black p-4 text-white flex lg:flex-row flex-col lg:justify-center text-left gap-2 py-6 mb-4">
+                            <Tab className="p-4 bg-blue-700 hover:bg-gray-300 cursor-pointer rounded hover:text-black hover:delay-100 duration-300">expert</Tab>
+                            <Tab className="p-4 bg-blue-700 hover:bg-gray-300 cursor-pointer rounded hover:text-black hover:delay-100 duration-300">cardiologist</Tab>
+                            <Tab className="p-4 bg-blue-700 hover:bg-gray-300 cursor-pointer rounded hover:text-black hover:delay-100 duration-300">dermatologist</Tab>
+                            <Tab className="p-4 bg-blue-700 hover:bg-gray-300 cursor-pointer rounded hover:text-black hover:delay-100 duration-300">orthopedic Surgeon</Tab>
+                            <Tab className="p-4 bg-blue-700 hover:bg-gray-300 cursor-pointer rounded hover:text-black hover:delay-100 duration-300">neurologist</Tab>
+                            <Tab className="p-4 bg-blue-700 hover:bg-gray-300 cursor-pointer rounded hover:text-black hover:delay-100 duration-300">endocrinologist</Tab>
+                            <Tab className="p-4 bg-blue-700 hover:bg-gray-300 cursor-pointer rounded hover:text-black hover:delay-100 duration-300">gynecologist</Tab>
+                        </TabList>
+                        <TabPanel>
+                            <div>
+                                <DoctorsList doctors={doctorsSpecial}></DoctorsList>
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div>
+                                <DoctorsList doctors={doctorsCardiologist}></DoctorsList>
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div>
+                                <DoctorsList doctors={doctorsDermatologist}></DoctorsList>
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div>
+                                <DoctorsList doctors={doctorsOrthopedic}></DoctorsList>
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div>
+                                <DoctorsList doctors={doctorsNeurologist}></DoctorsList>
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div>
+                                <DoctorsList doctors={doctorsEndocrinologist}></DoctorsList>
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div>
+                                <DoctorsList doctors={doctorsGynecologist}></DoctorsList>
+                            </div>
+                        </TabPanel>
+                    </Tabs>
+                </div>
+            </Container>
         </div>
     );
 };
--- src/pages/Services/Services/Services.jsx
+++ src/pages/Services/Services/Services.jsx
@@ -8,6 +8,7 @@ import { RiWomenLine } from "react-icons/ri";
 import { BiDonateBlood } from "react-icons/bi";
 import Appointment from "../../Shared/Appointment/Appointment";
 import HelmetProvide from "../../../component/HelmetProvide";
+import Container from "../../../component/Container/Container";
 
 const Services = () => {
     return (
@@ -18,59 +19,61 @@ const Services = () => {
                 subTitle={"Our Services"}
                 description={"The Doctors Clinic offers a variety of healthcare services to cater to patients of all ages. Patients can receive general consultations, which include comprehensive health check-ups and personalized medical advice. The clinic also provides specialized care in pediatrics, focusing on the healthy growth and development of children."}
             ></BannerShared>
-            <div className="lg:flex gap-6">
-                <div className="lg:w-1/2 px-4 lg:px-6">
-                    <img className="rounded-xl" src={doctorImg} alt="" />
-                </div>
-                <div className="lg:w-1/2 space-y-4 lg:text-justify px-4 lg:px-8">
-                    <h2 className="text-4xl text-orange-500 font-bold mt-8 lg:mt-0">Our Services</h2>
-                    <p className="leading-relaxed tracking-normal lg:text-justify text-left text-black">The Doctors Clinic offers a wide range of healthcare services to meet the needs of patients of all ages. From general consultations, where patients receive comprehensive health check-ups and personalized medical advice, to specialized care in pediatrics, ensuring children’s healthy growth and development, the clinic prioritizes patient well-being.</p>
-                    <div className="space-y-6 text-black">
-                        <div className="flex gap-4 text-left">
-                            <SlNotebook className="text-5xl font-bold text-blue-600"></SlNotebook>
-                            <div className="space-y-4">
-                                <h2 className="text-2xl font-bold">General Treatement</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus pariatur vel autem quas, dicta minus asperiores a dolorem fuga.</p>
+            <Container>
+                <div className="lg:flex gap-6 pt-10">
+                    <div className="lg:w-1/2">
+                        <img className="rounded-xl" src={doctorImg} alt="" />
+                    </div>
+                    <div className="lg:w-1/2 space-y-4 lg:text-justify ">
+                        <h2 className="text-4xl text-orange-500 font-bold mt-8 lg:mt-0">Our Services</h2>
+                        <p className="leading-relaxed tracking-normal lg:text-justify text-left text-black">The Doctors Clinic offers a wide range of healthcare services to meet the needs of patients of all ages. From general consultations, where patients receive comprehensive health check-ups and personalized medical advice, to specialized care in pediatrics, ensuring children’s healthy growth and development, the clinic prioritizes patient well-being.</p>
+                        <div className="space-y-6 text-black">
+                            <div className="flex gap-4 text-left">
+                                <SlNotebook className="text-5xl font-bold text-blue-600"></SlNotebook>
+                                <div className="space-y-4">
+                                    <h2 className="text-2xl font-bold">General Treatement</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus pariatur vel autem quas, dicta minus asperiores a dolorem fuga.</p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="flex gap-4 text-left">
-                            <FaHeart className="text-5xl font-bold text-blue-600"></FaHeart>
-                            <div className="space-y-4">
-                                <h2 className="text-2xl font-bold">Heart Surgery</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus pariatur vel autem quas, dicta minus asperiores a dolorem fuga.</p>
+                            <div className="flex gap-4 text-left">
+                                <FaHeart className="text-5xl font-bold text-blue-600"></FaHeart>
+                                <div className="space-y-4">
+                                    <h2 className="text-2xl font-bold">Heart Surgery</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus pariatur vel autem quas, dicta minus asperiores a dolorem fuga.</p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="flex gap-4 text-left">
-                            <LuBone className="text-5xl font-bold text-blue-600"></LuBone>
-                            <div className="space-y-4">
-                                <h2 className="text-2xl font-bold">Orthopedic Surgery</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus pariatur vel autem quas, dicta minus asperiores a dolorem fuga.</p>
+                            <div className="flex gap-4 text-left">
+                                <LuBone className="text-5xl font-bold text-blue-600"></LuBone>
+                                <div className="space-y-4">
+                                    <h2 className="text-2xl font-bold">Orthopedic Surgery</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus pariatur vel autem quas, dicta minus asperiores a dolorem fuga.</p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="flex gap-4 text-left">
-                            <FaBrain className="text-5xl font-bold text-blue-600"></FaBrain>
-                            <div className="space-y-4">
-                                <h2 className="text-2xl font-bold">Neuro Surgery</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus pariatur vel autem quas, dicta minus asperiores a dolorem fuga.</p>
+                            <div className="flex gap-4 text-left">
+                                <FaBrain className="text-5xl font-bold text-blue-600"></FaBrain>
+                                <div className="space-y-4">
+                                    <h2 className="text-2xl font-bold">Neuro Surgery</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus pariatur vel autem quas, dicta minus asperiores a dolorem fuga.</p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="flex gap-4 text-left">
-                            <RiWomenLine  className="text-5xl font-bold text-blue-600"></RiWomenLine >
-                            <div className="space-y-4">
-                                <h2 className="text-2xl font-bold">Gynecological Surgery</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus pariatur vel autem quas, dicta minus asperiores a dolorem fuga.</p>
+                            <div className="flex gap-4 text-left">
+                                <RiWomenLine className="text-5xl font-bold text-blue-600"></RiWomenLine >
+                                <div className="space-y-4">
+                                    <h2 className="text-2xl font-bold">Gynecological Surgery</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus pariatur vel autem quas, dicta minus asperiores a dolorem fuga.</p>
+                                </div>
                             </div>
-                        </div>
-                        <div className="flex gap-4 text-left">
-                            <BiDonateBlood  className="text-5xl font-bold text-blue-600"></BiDonateBlood >
-                            <div className="space-y-4">
-                                <h2 className="text-2xl font-bold">Blood Trasfusion</h2>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus pariatur vel autem quas, dicta minus asperiores a dolorem fuga.</p>
+                            <div className="flex gap-4 text-left">
+                                <BiDonateBlood className="text-5xl font-bold text-blue-600"></BiDonateBlood >
+                                <div className="space-y-4">
+                                    <h2 className="text-2xl font-bold">Blood Trasfusion</h2>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus pariatur vel autem quas, dicta minus asperiores a dolorem fuga.</p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </Container>
             <Appointment></Appointment>
         </div>
     );
